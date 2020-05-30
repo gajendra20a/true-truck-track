ef46422872aaef113f70f599bb4c44861a5b5280
gajendra20a <gajendra18862@gmail.com>
2020-05-31 00:05:32 +0530

    white space fix

--- src/app/services/truck-data.service.ts
+++ src/app/services/truck-data.service.ts
@@ -3,9 +3,9 @@ import {Injectable} from '@angular/core';
 import {HttpClient} from '@angular/common/http';
 import {BehaviorSubject, combineLatest, Observable} from 'rxjs';
 import {APITruck, APITruckWithStatus, TruckAPIResponse} from '../models/apiTruck';
-import {map, share, tap} from "rxjs/operators";
+import {map, share, tap} from 'rxjs/operators';
 
-const url = 'http://13.232.181.67:8080/tt/mobile/logistics/searchTrucks?auth-company=pbh&key=bmked1lou9ome2veh2jirgih5s&companyId=101&q-expand=true&q-include=lastRunningState,lastWaypoint'
+const url = 'http://13.232.181.67:8080/tt/mobile/logistics/searchTrucks?auth-company=pbh&key=bmked1lou9ome2veh2jirgih5s&companyId=101&q-expand=true&q-include=lastRunningState,lastWaypoint';
 const urlPost = 'http://13.232.181.67:8080/tt/mobile/logistics/createTruck?auth-company=pbh&key=bmked1lou9ome2veh2jirgih5s';
 export const total = 'Total';
 export const running = 'Running';
@@ -24,30 +24,30 @@ export class TruckDataService {
   constructor(
     private http: HttpClient
   ) {
-    this.loadTrucks()
+    this.loadTrucks();
   }
 
   public updateFilter(filter: string) {
-    console.log("I am getting called to set correct filter")
-    this.filter$$.next(filter)
+    console.log('I am getting called to set correct filter');
+    this.filter$$.next(filter);
   }
 
   private loadTrucks() {
     (this.http.get(url) as Observable<TruckAPIResponse>)
       .pipe(
         map((response) => {
-          const trucksWithStatus: APITruckWithStatus[] = []
+          const trucksWithStatus: APITruckWithStatus[] = [];
           response.data.forEach((truck) => {
             trucksWithStatus.push({
               ...truck,
               status: this.getTruckType(truck)
-            })
-          })
-          return trucksWithStatus
+            });
+          });
+          return trucksWithStatus;
         }),
       ).subscribe((trucks) => {
       this.trucks$$.next(trucks);
-    })
+    });
   }
 
   get trucks$(): Observable<APITruckWithStatus[]> {
@@ -61,15 +61,15 @@ export class TruckDataService {
       this.trucks$$.asObservable(),
       this.filter$$.asObservable(),
     ]).pipe(
-      tap(() => console.log("I am filtering data")),
+      tap(() => console.log('I am filtering data')),
       map(([trucks, filter]) => {
         if (filter === total) {
           return trucks;
         } else {
-          return trucks.filter((truck) => truck.status === filter)
+          return trucks.filter((truck) => truck.status === filter);
         }
       })
-    )
+    );
   }
 
 
@@ -79,7 +79,7 @@ export class TruckDataService {
       truckNumber: data.tNumber
     };
     return this.http.post<APITruck>(urlPost, response).subscribe(() => {
-      this.loadTrucks()
+      this.loadTrucks();
     });
   }
 
